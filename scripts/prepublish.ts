21b440158383dce16a9f85deadf4402ee67e82d0
Lance Tipton <lancetipton04@gmail.com>
2023-08-09 18:08:58 -0700

    feat: add d.ts to root

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -6,6 +6,8 @@ import { fileURLToPath } from 'node:url'
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 const rootDir = path.join(dirname, `../`)
 const typesDir = path.join(dirname, `../types`)
+const typesIdx = path.join(dirname, `../src/index.d.ts`)
+const typesIdxOut = path.join(dirname, `../.d.ts`)
 
 
 ;(async () => {
@@ -15,7 +17,9 @@ const typesDir = path.join(dirname, `../types`)
     cwd: typesDir,
   })
 
+  await cp(typesIdx, typesIdxOut)
   await Promise.all(typeFiles.map(async file => await cp(path.join(typesDir, file), path.join(rootDir, file))))
+
   console.log(`Copied type files to root`)
 
 })()
